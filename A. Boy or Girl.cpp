89c226c3c6aff542d15a86e8c30353bc68eb887a
Taj88 <taj16@cse.pstu.ac.bd>
2022-03-28 20:43:03 +0600

    wa

---
+++ A. Boy or Girl.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+
+using namespace std;
+int main(){
+    int count=0;
+string s;
+int x=0;
+cin>>s;
+sort(s.begin(),s.end());
+for(int i=0;i<s.length();i++){
+    if(s[i]!=s[i+1]){
+        count++;
+    }
+}
+
+if(count%2==0){
+    cout<<"CHAT WITH HER!\n";
+}
+else{
+    cout<<"IGNORE HIM!\n";
+}
+
+return 0;
+}
