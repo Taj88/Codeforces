27bee98f4f2e214b6d5af8e04c5077f7d92d1812
Taj88 <taj16@cse.pstu.ac.bd>
2022-04-04 01:30:39 +0600

    1

--- B_Team_Composition_Programmers_and_Mathematicians.cpp
+++ B_Team_Composition_Programmers_and_Mathematicians.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-
+int a,b,n;
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>a>>b;
+    int c=0;
+c=(a+b)/4;
+if(c>min(a,b)){
+    cout<<min(a,b)<<endl;
+}
+else{
+    cout<<c<<endl;
+}
+}
     return 0;
 }
